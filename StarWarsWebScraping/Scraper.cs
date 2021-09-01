c4243f9c45dbc145a35eb2045376939cfe859d11
Guzzard69 <khan7192020@gmail.com>
2021-09-01 15:19:25 -0400

    clean code

--- StarWarsWebScraping/Scraper.cs
+++ StarWarsWebScraping/Scraper.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using StarWarsWebScraping.Entities;
 using System;
 using System.Collections.Generic;
@@ -9,14 +10,13 @@ namespace StarWarsWebScraping
 {
     class Scraper
     {
-        private readonly ChromeDriver _driver;
+        private readonly RemoteWebDriver _driver;
 
         private readonly string WookiepeediaBaseUrl = "https://starwars.fandom.com/wiki";
 
-        public Scraper()
+        public Scraper(RemoteWebDriver driver)
         {
-            // Make sure ChromeDriver.exe is in the StarWarsWebScraping folder
-            _driver = new ChromeDriver(Environment.CurrentDirectory.Replace("\\bin\\Debug\\netcoreapp3.1", ""));
+            _driver = driver;
         }
 
         // Returns all characters and their information
@@ -30,6 +30,9 @@ namespace StarWarsWebScraping
                 {
                     var character = GetCharacter(x);
                     characters.Add(character);
+
+                    // Status Log
+                    Console.WriteLine($"Got: {character.CharacterName}");
                 }
                 catch (NotFoundException exception)
                 {
@@ -41,31 +44,6 @@ namespace StarWarsWebScraping
             _driver.Close();
             return characters;
         }
-        //public (List<Character> Characters, List<CharacterRelationship> Relationships) GetAllCharacters()
-        //{
-        //    var articleUrls = GetAllArticleUrls();
-
-        //    var characters = new List<Character>();
-        //    var relationships = new List<CharacterRelationship>();
-
-        //    foreach (var url in articleUrls)
-        //    {
-        //        try
-        //        {
-        //            var character = GetCharacter(url);
-        //            characters.Add(character.Character);
-        //            relationships.AddRange(character.Relationships);
-        //        }
-        //        catch (NotFoundException exception)
-        //        {
-        //            // GetCharacter throws NotFound if the article is not a character article.
-        //            continue;
-        //        }
-        //    }
-
-        //    _driver.Close();
-        //    return (characters, relationships);
-        //}
 
         // gets a characters page and returns their information
         private Character GetCharacter(string url)
@@ -84,27 +62,8 @@ namespace StarWarsWebScraping
 
         }
 
-        //private (Character Character, List<CharacterRelationship> Relationships) GetCharacter(string url)
-        //{
-        //    _driver.Navigate().GoToUrl(url);
-
-        //    // TODO: Fix this (checking if the article is a character article)
-        //    var infoTab = _driver.FindElementByXPath("//*[@id=\"mw-content-text\"]/div/aside");
-        //    if (!infoTab.Text.Contains("Gender")) throw new NotFoundException("This article is not a character article.");
-
-        //    var relationships = GetCharacterRelationships();
-
-        //    var character = new Character
-        //    {
-        //        CharacterName = _driver.FindElementById("firstHeading").Text,
-        //        Url = url,
-        //    };
-
-        //    return (character, relationships);
-        //}
-
         // Takes the div that represents the main text box for characters
-        private List<CharacterRelationship> GetCharacterRelationships(IWebElement characterInfoDiv)
+        private List<CharacterRelationship> GetCharacterRelationships(List<string> urls)
         {
             // TODO: Do this
             var relationships = new List<CharacterRelationship>();
@@ -118,16 +77,13 @@ namespace StarWarsWebScraping
             var urls = new List<string>();
 
             string nextPageUrl = $"{WookiepeediaBaseUrl}/Special:AllPages";
-            // TODO: reinstate this while loop
-            var page = GetArticlePage(nextPageUrl);
-            urls.AddRange(page.ArticleUrls);
 
-            //while (nextPageUrl != null)
-            //{
-            //    var page = GetArticlePage(nextPageUrl);
-            //    urls.AddRange(page.ArticleUrls);
-            //    nextPageUrl = page.NextPageUrl;
-            //}
+            while (nextPageUrl != null)
+            {
+                var page = GetArticlePage(nextPageUrl);
+                urls.AddRange(page.ArticleUrls);
+                nextPageUrl = page.NextPageUrl;
+            }
 
             Console.WriteLine("Finished Getting All Urls");
             return urls;
@@ -139,6 +95,7 @@ namespace StarWarsWebScraping
             _driver.Navigate().GoToUrl(url);
             var nextPageUrl = _driver.FindElementByXPath("//*[@id=\"mw-content-text\"]/div[2]/a[1]").GetAttribute("href");
 
+            // TODO: Use custom HTML parsing to speed this up.
             var articleUrls = _driver.FindElementByClassName("mw-allpages-chunk")
                 .FindElements(By.TagName("li"))
                 .ToList()

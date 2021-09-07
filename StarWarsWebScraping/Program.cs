31ed360efe5b3ab68bd20ae94b80262927a7d14e
Guzzard69 <khan7192020@gmail.com>
2021-09-07 13:02:44 -0400

    started testing parallel operations

--- StarWarsWebScraping/Program.cs
+++ StarWarsWebScraping/Program.cs
@@ -1,7 +1,11 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
 using StarWarsWebScraping.Entities;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StarWarsWebScraping
 {
@@ -16,20 +20,27 @@ namespace StarWarsWebScraping
             var rootPath = Environment.CurrentDirectory.Replace("\\bin\\Debug\\netcoreapp3.1", "");
             // Loads adblock to speed up page loading
             options.AddExtension(rootPath + "\\uBlock-Origin_v1.37.2.crx");
-            var driver = new ChromeDriver(rootPath, options);
-            
-            using var context = new StarWarsContext();
 
-            var scraper = new Scraper(driver, context);
+            var drivers = Enumerable.Range(0, 4).Select(x => new DriverWithId { Id = x, Driver = new ChromeDriver(rootPath, options) });
 
-            context.Characters.AddRange(scraper.GetAllCharacters());
-            context.SaveChanges();
+        //using var context = new StarWarsContext();
 
-            // Make sure GetRelationships is called after GetCharacters because relationships requires db data
-            // might want to fix this later
-            scraper.GetCharacterRelationships();
+        //var scraper = new Scraper(driver, context);
 
-            scraper.CloseDriver();
+        //context.Characters.AddRange(scraper.GetAllCharacters());
+        //context.SaveChanges();
+
+        //// Make sure GetRelationships is called after GetCharacters because relationships requires db data
+        //// might want to fix this later
+        //scraper.GetCharacterRelationships();
+
+        //scraper.CloseDriver();
         }
+    }
+
+    public class DriverWithId
+    {
+        public IWebDriver Driver { get; set; }
+        public int Id { get; set; }
     }
 }
